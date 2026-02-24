1ffd7ca44e9cfede7acd5f2c48ac02300e2fef0c
Abdulaziz Alotaibi <abdulaziz_sultan1@hotmail.com>
2026-02-24 06:46:37 +0000

    Task 6: prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	long long int c;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));

fbadadbbbfaccb1e55cbb03c5a8c400d59974812
Abdulaziz Alotaibi <Abdulaziz_sultan1@hotmail.com>
2026-02-26 21:19:26 +0000

    Task 5:  a function that prints the sign of a number

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,5 +6,6 @@ void print_alphabet(void); /* Task1  prototype */
 void print_alphabet_x10(void);    /* Task 2 */
 int _islower(int c);          /* Task 3 */
 int _isalpha(int c);          /* Task 4 */
+int print_sign(int n);    /* Task 5*/
 
 #endif 

243983c3d8abfacf32d224ec6c1bcf72066c7143
Abdulaziz Alotaibi <Abdulaziz_sultan1@hotmail.com>
2026-02-26 21:43:50 +0000

    Task 6: add _abs function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void print_alphabet(void); /* Task1  prototype */
 void print_alphabet_x10(void);    /* Task 2 */
 int _islower(int c);          /* Task 3 */
 int _isalpha(int c);          /* Task 4 */
-int print_sign(int n);    /* Task 5*/
+int print_sign(int n);  /* Task 5*/
+int _abs(int);
 
 #endif 

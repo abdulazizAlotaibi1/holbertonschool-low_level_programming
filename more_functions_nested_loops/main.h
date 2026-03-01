2465dc481b3166dc73933057e118369f6e6687bf
Abdulaziz Alotaibi <Abdulaziz_sultan1@hotmail.com>
2026-03-01 21:51:26 +0000

    Task 3: a function that prints the numbers, from 0 to 9

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c); /*Task 0 */
 int _isdigit(int c); /* Task 1*/
 int mul(int a, int b); /* Task 2 */
+void print_numbers(void); /* Task 3*/
 
 #endif

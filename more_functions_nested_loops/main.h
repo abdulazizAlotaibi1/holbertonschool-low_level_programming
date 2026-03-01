27b1e47e5200285c8b7593004ea6051d591f110a
Abdulaziz Alotaibi <Abdulaziz_sultan1@hotmail.com>
2026-03-01 22:41:51 +0000

    Task 5: a function that prints 10 times the numbers, from 0 to 14

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _isdigit(int c); /* Task 1*/
 int mul(int a, int b); /* Task 2 */
 void print_numbers(void); /* Task 3*/
 void print_most_numbers(void); /* Task 4*/ 
+void more_numbers(void);
 
 #endif

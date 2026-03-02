6faaf7e88ad931781b0f66e194fa65afeca2dfba
Abdulaziz Alotaibi <Abdulaziz_sultan1@hotmail.com>
2026-03-02 20:04:20 +0000

    Task 9 : FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100,
+ *             replacing multiples of 3 with Fizz,
+ *             multiples of 5 with Buzz,
+ *             multiples of both with FizzBuzz
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+	return (0);
+}

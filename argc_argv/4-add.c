56b62ad431354f01fa0144f1c190af230f2587ba
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-03-14 07:10:52 +0300

    Create 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers passed as arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 if error (non-digit input)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	char *p;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		p = argv[i];
+		while (*p)
+		{
+			if (*p < '0' || *p > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			p++;
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}

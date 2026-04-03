5bd66fc69704dfb1dacf2778af7f14fe8318a1d3
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-04-03 21:28:45 +0300

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,50 +11,47 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    int i, len;
+	dog_t *d;
+	int i, len;
 
-    /* Allocate memory for the dog structure */
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-    /* Copy name */
-    if (name != NULL)
-    {
-        for (len = 0; name[len]; len++)  /* calculate length manually */
-            ;
-        d->name = malloc(len + 1);
-        if (d->name == NULL)
-        {
-            free(d);
-            return (NULL);
-        }
-        for (i = 0; i <= len; i++)  /* copy character by character including '\0' */
-            d->name[i] = name[i];
-    }
-    else
-        d->name = NULL;
+	if (name != NULL)
+	{
+		for (len = 0; name[len]; len++)
+			;
+		d->name = malloc(len + 1);
+		if (d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+		for (i = 0; i <= len; i++)
+			d->name[i] = name[i];
+	}
+	else
+		d->name = NULL;
 
-    d->age = age;
+	d->age = age;
 
-    /* Copy owner */
-    if (owner != NULL)
-    {
-        for (len = 0; owner[len]; len++)
-            ;
-        d->owner = malloc(len + 1);
-        if (d->owner == NULL)
-        {
-            free(d->name);
-            free(d);
-            return (NULL);
-        }
-        for (i = 0; i <= len; i++)
-            d->owner[i] = owner[i];
-    }
-    else
-        d->owner = NULL;
+	if (owner != NULL)
+	{
+		for (len = 0; owner[len]; len++)
+			;
+		d->owner = malloc(len + 1);
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+		for (i = 0; i <= len; i++)
+			d->owner[i] = owner[i];
+	}
+	else
+		d->owner = NULL;
 
-    return (d);
+	return (d);
 }

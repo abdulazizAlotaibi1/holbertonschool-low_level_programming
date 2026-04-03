679c903992a898687c28518a8060b5c467f6d642
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-04-03 21:31:13 +0300

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,56 +2,58 @@
 #include "dog.h"
 
 /**
+ * _strdup - duplicates a string
+ * @str: string to copy
+ * Return: pointer to new string, or NULL if failed
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+		;
+
+	copy = malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+
+/**
  * new_dog - creates a new dog_t instance
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to new dog_t, or NULL on failure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: pointer to new dog_t, or NULL if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	int i, len;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	if (name != NULL)
-	{
-		for (len = 0; name[len]; len++)
-			;
-		d->name = malloc(len + 1);
-		if (d->name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len; i++)
-			d->name[i] = name[i];
-	}
-	else
-		d->name = NULL;
-
+	d->name = _strdup(name);
+	d->owner = _strdup(owner);
 	d->age = age;
 
-	if (owner != NULL)
+	if ((name != NULL && d->name == NULL) ||
+	    (owner != NULL && d->owner == NULL))
 	{
-		for (len = 0; owner[len]; len++)
-			;
-		d->owner = malloc(len + 1);
-		if (d->owner == NULL)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= len; i++)
-			d->owner[i] = owner[i];
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
-	else
-		d->owner = NULL;
 
 	return (d);
 }

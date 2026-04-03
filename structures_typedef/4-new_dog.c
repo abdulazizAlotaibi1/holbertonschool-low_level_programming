7fd50eaa51c3e019d734006b464b95b797d540e3
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-04-03 21:26:49 +0300

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,48 +1,57 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * new_dog - creates a new dog_t instance
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog, or NULL on failure
+ * Return: pointer to new dog_t, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *d;
+    int i, len;
 
+    /* Allocate memory for the dog structure */
     d = malloc(sizeof(dog_t));
     if (d == NULL)
         return (NULL);
 
+    /* Copy name */
     if (name != NULL)
     {
-        d->name = malloc(strlen(name) + 1);
+        for (len = 0; name[len]; len++)  /* calculate length manually */
+            ;
+        d->name = malloc(len + 1);
         if (d->name == NULL)
         {
             free(d);
             return (NULL);
         }
-        strcpy(d->name, name);
+        for (i = 0; i <= len; i++)  /* copy character by character including '\0' */
+            d->name[i] = name[i];
     }
     else
         d->name = NULL;
 
     d->age = age;
 
+    /* Copy owner */
     if (owner != NULL)
     {
-        d->owner = malloc(strlen(owner) + 1);
+        for (len = 0; owner[len]; len++)
+            ;
+        d->owner = malloc(len + 1);
         if (d->owner == NULL)
         {
             free(d->name);
             free(d);
             return (NULL);
         }
-        strcpy(d->owner, owner);
+        for (i = 0; i <= len; i++)
+            d->owner[i] = owner[i];
     }
     else
         d->owner = NULL;

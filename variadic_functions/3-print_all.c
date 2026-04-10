1a802a66028945ffa509efca11fd4fe63e6a1285
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-04-11 01:19:42 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,42 +3,72 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char
+ */
+void print_char(va_list ap, char *sep)
+{
+	printf("%s%c", sep, va_arg(ap, int));
+}
+
+/**
+ * print_int - prints an int
+ */
+void print_int(va_list ap, char *sep)
+{
+	printf("%s%d", sep, va_arg(ap, int));
+}
+
+/**
+ * print_float - prints a float
+ */
+void print_float(va_list ap, char *sep)
+{
+	printf("%s%f", sep, va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string
+ */
+void print_string(va_list ap, char *sep)
+{
+	char *str = va_arg(ap, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s%s", sep, str);
+}
+
+/**
  * print_all - prints anything
  * @format: list of types
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int i = 0;
-	char *str;
+	int i = 0, j;
 	char *sep = "";
+	print_t ops[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
 	va_start(ap, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		while (ops[j].type)
 		{
-			printf("%s%c", sep, va_arg(ap, int));
-			sep = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(ap, int));
-			sep = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(ap, double));
-			sep = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			sep = ", ";
+			if (format[i] == ops[j].type[0])
+			{
+				ops[j].f(ap, sep);
+				sep = ", ";
+			}
+			j++;
 		}
 		i++;
 	}

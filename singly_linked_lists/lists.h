e4756ac5fe77f97434777fce0d0422c6bfc2745e
abdulazizAlotaibi1 <abdulaziz_sultan1@hotmail.com>
2026-04-25 05:40:32 +0300

    Update lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,12 +11,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/* Function prototype */
 size_t print_list(const list_t *h);
 
 #endif
